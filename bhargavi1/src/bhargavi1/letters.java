7037c2490e172dfe6e3c550262062cff3193e2b7
Bhavana5599 <p.bhavanareddy99@gmail.com>
2020-07-21 11:37:43 +0530

    commit

---
+++ bhargavi1/src/bhargavi1/letters.java
@@ -0,0 +1,19 @@
+package bhargavi1;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class letters {
+	public static List<String> stringPatternList(List<String> list){
+		List<String> count = list.stream().filter(x -> x.startsWith("a")).filter(x -> x.length()<=3).collect(Collectors.toList());
+		return count;
+
+}
+	public static void main(String args[]) {
+		List<String> strList = Arrays.asList("abc", "abcd", "aef","bcd", "bcde", "defg", "jk"); 
+		List<String> str=stringPatternList(strList);
+		System.out.println(str);
+
+	}
+}
